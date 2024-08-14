b6d4afd04236b94d3005d381d5c7c6f1134f2faa
HiranSasitha <hiransasitha123@gamil.com>
2024-08-14 20:33:28 +0530

    project initialize

---
+++ src/main/java/com/example/abc_restaurant_system/AbcRestaurantSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.abc_restaurant_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcRestaurantSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcRestaurantSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/abc_restaurant_system/AbcRestaurantSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.abc_restaurant_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcRestaurantSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
