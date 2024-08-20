e0a3bc7206e6d876093604ebd2b02742fc37d185
HiranSasitha <hiransasitha123@gamil.com>
2024-08-20 21:14:20 +0530

    feat:getall branches

--- src/main/java/com/example/abc_restaurant_system/controller/ItemController.java
+++ src/main/java/com/example/abc_restaurant_system/controller/ItemController.java
@@ -1,9 +1,36 @@
 package com.example.abc_restaurant_system.controller;
 
+import com.example.abc_restaurant_system.dto.CategoryDto;
+import com.example.abc_restaurant_system.dto.ItemDto;
+import com.example.abc_restaurant_system.service.ItemService;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/item")
 public class ItemController {
+
+    @Autowired
+    private ItemService itemService;
+
+    @PostMapping("/create")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> saveCategory(@RequestBody ItemDto itemDto){
+        JSONObject jsonObject = new JSONObject();
+
+        String msg = itemService.save(itemDto);
+
+        jsonObject.put("msg",msg);
+
+        return new ResponseEntity<>(jsonObject.toString(), HttpStatus.CREATED);
+    }
+
+
 }
---
+++ src/main/java/com/example/abc_restaurant_system/service/BranchService.java
@@ -0,0 +1,12 @@
+package com.example.abc_restaurant_system.service;
+
+import com.example.abc_restaurant_system.entity.Branch;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface BranchService {
+
+    List<Branch> getAll();
+}
